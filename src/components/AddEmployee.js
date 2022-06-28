c70f2f5ff033dff7f405b456e5069356e3c43ed1
prabath1998 <prabhathudayanga1998@gmail.com>
2022-06-28 15:39:03 +0530

    add state

--- src/components/AddEmployee.js
+++ src/components/AddEmployee.js
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const AddEmployee = () => {
+
+
+    const [employee, setEmployee] = useState({
+        id:"",
+        firstName:"",
+        lastName:"",
+        emailId:"",
+    });
+
+
   return (
     <div className='flex max-w-2xl shadow border-b mx-auto'>
         <div className='px-8 py-8'>
@@ -9,15 +19,15 @@ const AddEmployee = () => {
             </div>
             <div className='items-center justify-center h-16 w-full my-4'>
                 <label className='block text-gray-600 text-sm font-normal'>First Name</label>
-                <input type="text" className='h-10 w-96 border mt-2 px-2 py-2'></input>
+                <input type="text" className='h-10 w-96 border mt-2 px-2 py-2' name='firstName' value={employee.firstName}></input>
             </div>
             <div className='items-center justify-center h-16 w-full my-4'>
                 <label className='block text-gray-600 text-sm font-normal'>Last Name</label>
-                <input type="text" className='h-10 w-96 border mt-2 px-2 py-2'></input>
+                <input type="text" className='h-10 w-96 border mt-2 px-2 py-2' name='lastName' value={employee.lastName}></input>
             </div>
             <div className='items-center justify-center h-16 w-full my-4'>
                 <label className='block text-gray-600 text-sm font-normal'>Email</label>
-                <input type="email" className='h-10 w-96 border mt-2 px-2 py-2'></input>
+                <input type="email" className='h-10 w-96 border mt-2 px-2 py-2' name='emailId' value={employee.emailId}></input>
             </div>
 
             <div className='items-center justify-center h-16 w-full my- space-x-4 pt-4'>
