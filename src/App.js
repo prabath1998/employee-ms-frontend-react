afd34b4baa3aa3d1dd71b7ca7fdd4743355c727c
prabath1998 <prabhathudayanga1998@gmail.com>
2022-06-28 18:44:45 +0530

    add routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-
-import './App.css';
-import AddEmployee from './components/AddEmployee';
-import Navbar from './components/Navbar';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import AddEmployee from "./components/AddEmployee";
+import EmployeeList from "./components/EmployeeList";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-   <>
-    <Navbar/>
-    <AddEmployee/>
-   </>
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<EmployeeList/>}></Route>
+          <Route index element={<EmployeeList/>}></Route>
+          <Route path="/employeeList" element={<EmployeeList/>}></Route>
+          <Route path="/addEmployee" element={<AddEmployee/>}></Route>
+        </Routes>
+        
+      </BrowserRouter>
+    </>
   );
 }
 
