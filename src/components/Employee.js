8500e9b2558603e2fd9d9b56080536ed869b9cff
prabath1998 <prabhathudayanga1998@gmail.com>
2022-06-30 19:26:25 +0530

    add onclick in employee

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -14,8 +14,8 @@ const Employee = ({employee, deleteEmployee}) => {
     </td>
     <td className="text-right px-6 py-4 whitespace-nowrap font-medium text-sm">
       <a
-        href="#"
-        className="text-indigo-600 hover:text-indigo-800 px-4"
+        onClick={(e,id)=>editEmployee(e,id)}
+        className="text-indigo-600 hover:text-indigo-800 px-4 hover:cursor-pointer"
       >
         Edit
       </a>
