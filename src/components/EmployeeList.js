267be84a31420fd7b431ac702824ef7a7c24a71c
prabath1998 <prabhathudayanga1998@gmail.com>
2022-06-30 10:54:18 +0530

    add employee component

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import EmployeeService from "../services/EmployeeService";
+import Employee from "./Employee";
 
 const EmployeeList = () => {
   const navigate = useNavigate();
@@ -52,28 +53,7 @@ const EmployeeList = () => {
           {!loading && (
             <tbody className="bg-white">
               {employees.map((employee) => (
-                <tr key={employee.id}>
-                  <td className="text-left px-6 py-4 whitespace-nowrap">
-                    <div className="text-sm text-gray-500">{employee.firstName}</div>
-                  </td>
-                  <td className="text-left px-6 py-4 whitespace-nowrap">
-                    <div className="text-sm text-gray-500">{employee.lastName}</div>
-                  </td>
-                  <td className="text-left px-6 py-4 whitespace-nowrap">
-                    <div className="text-sm text-gray-500">{employee.emailId}</div>
-                  </td>
-                  <td className="text-right px-6 py-4 whitespace-nowrap font-medium text-sm">
-                    <a
-                      href="#"
-                      className="text-indigo-600 hover:text-indigo-800 px-4"
-                    >
-                      Edit
-                    </a>
-                    <a href="#" className="text-red-600 hover:text-red-800">
-                      Delete
-                    </a>
-                  </td>
-                </tr>
+               <Employee employee={employee}></Employee>
               ))}
             </tbody>
           )}
